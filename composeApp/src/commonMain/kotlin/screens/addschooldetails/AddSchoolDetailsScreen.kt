b63ddc7ea7f45c05f468a95f5875cf1497418cd5
aravindh2106 <aravindhan@logicsoft.co.in>
2024-05-24 16:55:19 +0530

    after saved successfully navigate back

--- composeApp/src/commonMain/kotlin/screens/addschooldetails/AddSchoolDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/addschooldetails/AddSchoolDetailsScreen.kt
@@ -14,11 +14,16 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarDuration
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -51,141 +56,159 @@ fun AddSchoolDetails(schoolDao: SchoolDao, onBackClick: () -> Unit) {
     val schoolErrorView = addSchoolDetailsScreenModel.schoolErrorView.collectAsState().value
     val isSuccess = addSchoolDetailsScreenModel.isSuccess.collectAsState().value
     val offset = Offset(5.0f, 10.0f)
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        IconButton(
-            onClick = onBackClick,
-            modifier = Modifier
-                .align(Alignment.TopStart)
-        ) {
-            Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
         }
-        Column(
-            Modifier
-                .fillMaxWidth().fillMaxHeight(),
-            horizontalAlignment = Alignment.CenterHorizontally
+    ) { contentPadding ->
+        LaunchedEffect(isSuccess) {
+            if (isSuccess) {
+                onBackClick()
+                snackbarHostState.showSnackbar(
+                    message = "School details successfully saved!",
+                    duration = SnackbarDuration.Short
+                )
+
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
         ) {
-            Text(
-                "Fill the school details",
-                modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
-                color = Color.Blue,
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold,
-                style = TextStyle(
-                    fontSize = 24.sp,
-                    shadow = Shadow(
-                        color = Color.Gray, offset = offset, blurRadius = 3f
+            IconButton(
+                onClick = onBackClick,
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+            ) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, "Back")
+            }
+            Column(
+                Modifier
+                    .fillMaxWidth().fillMaxHeight(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    "Fill the school details",
+                    modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
+                    color = Color.Blue,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold,
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        shadow = Shadow(
+                            color = Color.Gray, offset = offset, blurRadius = 3f
+                        )
                     )
                 )
-            )
 
-            OutlinedTextField(
-                value = schoolView.schoolName ?: "",
-                onValueChange = {
-                    if (schoolErrorView.isSchoolNameError) {
-                        addSchoolDetailsScreenModel.updateErrorState(
-                            schoolErrorView.copy(
-                                isSchoolNameError = false
+                OutlinedTextField(
+                    value = schoolView.schoolName ?: "",
+                    onValueChange = {
+                        if (schoolErrorView.isSchoolNameError) {
+                            addSchoolDetailsScreenModel.updateErrorState(
+                                schoolErrorView.copy(
+                                    isSchoolNameError = false
+                                )
                             )
-                        )
-                    }
-                    addSchoolDetailsScreenModel.updateData(schoolView.copy(schoolName = it))
-                },
-                placeholder = { Text(text = "School name") },
-                modifier = Modifier
-                    .padding(8.dp),
-                trailingIcon = {
-                    if (schoolErrorView.isSchoolNameError) {
-                        Icon(Icons.Filled.Info, "Error", tint = MaterialTheme.colors.error)
-                    }
-                },
-                isError = schoolErrorView.isSchoolNameError
-            )
-            if (schoolErrorView.isSchoolNameError) {
-                Text(
-                    textAlign = TextAlign.End,
-                    text = schoolErrorView.schoolNameError!!,
-                    color = MaterialTheme.colors.error,
-                    style = MaterialTheme.typography.body1,
+                        }
+                        addSchoolDetailsScreenModel.updateData(schoolView.copy(schoolName = it))
+                    },
+                    placeholder = { Text(text = "School name") },
+                    modifier = Modifier
+                        .padding(8.dp),
+                    trailingIcon = {
+                        if (schoolErrorView.isSchoolNameError) {
+                            Icon(Icons.Filled.Info, "Error", tint = MaterialTheme.colors.error)
+                        }
+                    },
+                    isError = schoolErrorView.isSchoolNameError
                 )
-            }
-            Spacer(Modifier.width(8.dp))
-            OutlinedTextField(
-                value = schoolView.description ?: "",
-                onValueChange = {
-                    if (schoolErrorView.isDescriptionNameError) {
-                        addSchoolDetailsScreenModel.updateErrorState(
-                            schoolErrorView.copy(
-                                isDescriptionNameError = false
+                if (schoolErrorView.isSchoolNameError) {
+                    Text(
+                        textAlign = TextAlign.End,
+                        text = schoolErrorView.schoolNameError!!,
+                        color = MaterialTheme.colors.error,
+                        style = MaterialTheme.typography.body1,
+                    )
+                }
+                Spacer(Modifier.width(8.dp))
+                OutlinedTextField(
+                    value = schoolView.description ?: "",
+                    onValueChange = {
+                        if (schoolErrorView.isDescriptionNameError) {
+                            addSchoolDetailsScreenModel.updateErrorState(
+                                schoolErrorView.copy(
+                                    isDescriptionNameError = false
+                                )
                             )
-                        )
-                    }
-                    addSchoolDetailsScreenModel.updateData(schoolView.copy(description = it))
-                },
-                placeholder = { Text(text = "Description") },
-                trailingIcon = {
-                    if (schoolErrorView.isDescriptionNameError) {
-                        Icon(Icons.Filled.Info, "Error", tint = MaterialTheme.colors.error)
-                    }
-                },
-                isError = schoolErrorView.isDescriptionNameError,
-                modifier = Modifier
-                    .padding(8.dp)
-            )
-            if (schoolErrorView.isDescriptionNameError) {
-                Text(
-                    textAlign = TextAlign.End,
-                    text = schoolErrorView.descriptionError!!,
-                    color = MaterialTheme.colors.error,
-                    style = MaterialTheme.typography.body1,
+                        }
+                        addSchoolDetailsScreenModel.updateData(schoolView.copy(description = it))
+                    },
+                    placeholder = { Text(text = "Description") },
+                    trailingIcon = {
+                        if (schoolErrorView.isDescriptionNameError) {
+                            Icon(Icons.Filled.Info, "Error", tint = MaterialTheme.colors.error)
+                        }
+                    },
+                    isError = schoolErrorView.isDescriptionNameError,
+                    modifier = Modifier
+                        .padding(8.dp)
                 )
-            }
-            Spacer(Modifier.width(8.dp))
-            OutlinedTextField(
-                value = schoolView.city ?: "",
-                onValueChange = {
-                    if (schoolErrorView.isCityNameError) {
-                        addSchoolDetailsScreenModel.updateErrorState(
-                            schoolErrorView.copy(
-                                isCityNameError = false
+                if (schoolErrorView.isDescriptionNameError) {
+                    Text(
+                        textAlign = TextAlign.End,
+                        text = schoolErrorView.descriptionError!!,
+                        color = MaterialTheme.colors.error,
+                        style = MaterialTheme.typography.body1,
+                    )
+                }
+                Spacer(Modifier.width(8.dp))
+                OutlinedTextField(
+                    value = schoolView.city ?: "",
+                    onValueChange = {
+                        if (schoolErrorView.isCityNameError) {
+                            addSchoolDetailsScreenModel.updateErrorState(
+                                schoolErrorView.copy(
+                                    isCityNameError = false
+                                )
                             )
-                        )
-                    }
-                    addSchoolDetailsScreenModel.updateData(schoolView.copy(city = it))
-                },
-                placeholder = { Text(text = "City") },
-                modifier = Modifier
-                    .padding(8.dp),
-                trailingIcon = {
-                    if (schoolErrorView.isCityNameError) {
-                        Icon(Icons.Filled.Info, "Error", tint = MaterialTheme.colors.error)
-                    }
-                },
-                isError = schoolErrorView.isCityNameError
-            )
-            if (schoolErrorView.isCityNameError) {
-                Text(
-                    textAlign = TextAlign.End,
-                    text = schoolErrorView.cityError!!,
-                    color = MaterialTheme.colors.error,
-                    style = MaterialTheme.typography.body1,
+                        }
+                        addSchoolDetailsScreenModel.updateData(schoolView.copy(city = it))
+                    },
+                    placeholder = { Text(text = "City") },
+                    modifier = Modifier
+                        .padding(8.dp),
+                    trailingIcon = {
+                        if (schoolErrorView.isCityNameError) {
+                            Icon(Icons.Filled.Info, "Error", tint = MaterialTheme.colors.error)
+                        }
+                    },
+                    isError = schoolErrorView.isCityNameError
                 )
-            }
-            Spacer(Modifier.width(8.dp))
+                if (schoolErrorView.isCityNameError) {
+                    Text(
+                        textAlign = TextAlign.End,
+                        text = schoolErrorView.cityError!!,
+                        color = MaterialTheme.colors.error,
+                        style = MaterialTheme.typography.body1,
+                    )
+                }
+                Spacer(Modifier.width(8.dp))
 
-            Button(
-                onClick = {
-                    addSchoolDetailsScreenModel.saveSchool()
-                },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = Color(0xFFe8f5c4), // Set the background color
-                    contentColor = Color.Black   // Set the text color
-                )
-            ) {
-                Text("SAVE", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+                Button(
+                    onClick = {
+                        addSchoolDetailsScreenModel.saveSchool()
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color(0xFFe8f5c4), // Set the background color
+                        contentColor = Color.Black   // Set the text color
+                    )
+                ) {
+                    Text("SAVE", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+                }
             }
         }
     }
+
 }
